c4b8a2079a58257cde7781d2d16b21f72553a5f5
baade <zz@baade.org>
2016-12-16 11:36:43 +0800

    gauss

---
+++ drone-gauss/src/main/java/org/baade/drone/gauss/App.java
@@ -0,0 +1,13 @@
+package org.baade.drone.gauss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
